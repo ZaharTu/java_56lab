376586d093a29aa4272be1869b3b97a968bb8b5a
Туманов Захар Андреевич ИП-514 <zahar.tumanow@yandex.ru>
2025-10-22 22:18:49 +0700

    Add method getCarByBrendAndYearOperational

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Car[] cars = {
                 new Car(1, "Toyota", "Camry", 2020, "White", 15000, "A123BC77"),
                 new Car(2, "Toyota", "Madina", 2020, "Black", 45000, "B456DE77"),
@@ -9,6 +11,7 @@ public class Main{
         };
         System.out.println(Arrays.toString(getCarByBrend(cars, "Toyota")));
         System.out.println(Arrays.toString(getCarByBrend(cars, "123")));
+        System.out.println(Arrays.toString(getCarByBrendAndYearOperational(cars, "Toyota", 4)));
     }
     public static Car[] getCarByBrend(Car[] cars, String brend){
         int count=0;
@@ -25,5 +28,29 @@ public class Main{
             }
         }
         return ret_cars;
+    }
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+        final int CURRENT_YEAR = 2025;
+        int count = 0;
+        for (Car car : cars) {
+            if (car.getBrend().equals(brend)) {
+                int operationalYears = CURRENT_YEAR - car.getYear();
+                if (operationalYears > years) {
+                    count++;
+                }
+            }
+        }
+        Car[] result = new Car[count];
+        int index = 0;
+        for (Car car : cars) {
+            if (car.getBrend().equals(brend)) {
+                int operationalYears = CURRENT_YEAR - car.getYear();
+                if (operationalYears > years) {
+                    result[index++] = car;
+                }
+            }
+        }
+
+        return result;
     }
 }
