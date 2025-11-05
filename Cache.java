aee831c53c648d5fd5d39bea2ac6b5d4c1e30be2
Туманов Захар Андреевич ИП-514 <zahar.tumanow@yandex.ru>
2025-11-05 18:48:59 +0700

    Создан класс и конструктор Cache

---
+++ Cache.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Cache<T> {
+
+    private final int n;
+    private final Deque<T> array_deque;
+
+    public Cache(int n){
+        if(n<0){
+            throw new IllegalArgumentException("длина кеша должна быть больше 0");
+        }
+        this.n=n;
+        array_deque = new ArrayDeque<>(n);
+    }
+
+
+}
